5ff01c3a34a920f8dba8d2ee483a2fa3c7459639
Nick Foden <nickfoden@gmail.com>
2020-02-29 17:37:49 -0500

    Updating to use color and size

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -6,18 +6,20 @@ const LoaderWrapper = styled.div`
 	position: absolute;
 	top: 50%;
 	left: 50%;
-	border: 16px solid ${(props) => props.border};
-	border-top: 16px solid ${(props) => props.borderTop};
+	border: 16px solid #f3f3f3;
+	border-top: 16px solid #f05327;
+	${color}
 	border-radius: 50%;
 	width: 120px;
 	height: 120px;
 	animation: spin 2s linear infinite;
 	margin-top: -60px;
 	margin-left: -60px;
+	${space}
 `;
 
-export function Loader(border = '#f3f3f3', borderTop = '#f05327') {
-	return <LoaderWrapper border={border} borderTop={borderTop} />;
+export function Loader(props: any) {
+	return <LoaderWrapper {...props} />;
 }
 
 export default Loader;
