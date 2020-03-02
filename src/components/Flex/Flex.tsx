489c07861d88432aeeb166a250a04a934196aabb
Nick Foden <nickfoden@gmail.com>
2020-03-02 08:48:38 -0500

    Add prop types

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+//import PropTypes from 'prop-types';
 import { color, flexbox, space } from 'styled-system'
 
 const StyledFlex= styled.div`
